b716e59d50308e198b25552817772283dcbbf5d3
Declan <decofeco29@gmail.com>
2018-11-26 18:56:35 +0000

    Create car service

---
+++ src/app/car.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { Car } from './car';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+
+  constructor(private http: HttpClient) { }
+}
